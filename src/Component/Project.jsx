cb1f29c67fd309bae4fd015e6bdd1daa597534ed
deepa9360 <deeparajangam9@gmail.com>
2024-02-08 18:40:49 +0530

    fixed

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -3,41 +3,35 @@ import React, { useState } from "react";
 
 
 function Project (){
-  const[text,settext]=useState([])
-  const[btn,setbtn]=useState("")
-     
+  const[text,settext]=useState("")
+  const[btn,setbtn]=useState([])
+
 const handleadd=(e)=>{
       settext(e.target.value)
 }
   const handleiterm=(e)=>{
-       setbtn([...text,btn ])
+       setbtn([...btn,text])
 
   }
-
-
+   
     return(
 
       <div>
-        <div>
-      <h1>Grocery Bud</h1>
-      <input type="text" onChange={handleadd}/>
-      <button onClick={handleiterm}>Add Iterm</button>
 
-     <h2>add:{text} </h2>
+      <h1>Grocery Bud</h1>
+      <input type="text"onChange={handleadd}/>
+      <button onClick={handleiterm}>Add Iterm</button>
+      <div>
+     <h1>Add:{text}</h1>
+     {btn.map((sd,i)=>(
+      <div key={i}>
+        <h1>{sd}</h1>
+      </div>
+     ))}
+    
         </div>
       </div>
 
-
-
     )
-
-
-
 }
-
-export default Project
-
-
-
-
-
+export default Project;
