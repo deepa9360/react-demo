4505457b4014159eb1057f39afa28335051cc3c5
deepa9360 <deeparajangam9@gmail.com>
2024-02-14 12:19:34 +0530

    add

---
+++ src/Component/Regform.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+function Regform() {
+
+  const {
+
+    register,handleSubmit
+  } = useForm()
+
+  const onsubmit = (data) => {
+    console.log(data);
+    
+  }
+
+  return (
+
+    <div>
+
+      <form>
+        <input type="text" placeholder="enter your name" {...register("first name")} />
+        <br />
+        <br />
+        <input type="text" placeholder="enter your email" {...register("email")} />
+        <br />
+        <br />
+
+        <input type="text" placeholder="enter your age" {...register("age")} />
+        <br />
+        <br />
+
+        <input type="text" placeholder="enter your d.o.b"{...register("d.o.b")} />
+        <br />
+        <br/>
+
+        <button onClick={handleSubmit(onsubmit)}>submit</button>
+
+
+      </form>
+
+    </div>
+  )
+}
+
+export default Regform;
+
